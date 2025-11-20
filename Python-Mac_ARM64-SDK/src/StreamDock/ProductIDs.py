255dde16ea1c45ff4c4dce92d58f03e8f80bbed1
JKWTCN <jkwtcn@icloud.com>
2025-11-20 15:34:50 +0800

    Python SDK: 添加 N4Pro;

--- Python-Mac_ARM64-SDK/src/StreamDock/ProductIDs.py
+++ Python-Mac_ARM64-SDK/src/StreamDock/ProductIDs.py
@@ -2,6 +2,7 @@ class USBVendorIDs:
     """
     USB Vendor IDs for known StreamDock devices.
     """
+
     USB_VID_293 = 0x5500
     USB_VID_293s = 0x5548
     USB_PID_293V3 = 0x6603
@@ -13,12 +14,15 @@ class USBVendorIDs:
     USB_VIDN4EN = 0x6603
     USB_VIDN1EN = 0x6603
     USB_VIDN1 = 0x6603
+    USB_VIDN4PRO = 0x5548
+    USB_VIDN4PROE = 0x5548
 
 
 class USBProductIDs:
     """
     USB Product IDs for known StreamDock devices.
     """
+
     USB_PID_STREAMDOCK_293 = 0x1001
     USB_PID_STREAMDOCK_293s = 0x6670
     USB_PID_STREAMDOCK_293V3 = 0x1005
@@ -32,6 +36,9 @@ class USBProductIDs:
     USB_PID_STREAMDOCK_N4EN = 0x1007
     USB_PID_STREAMDOCK_N1EN = 0x1000
     USB_PID_STREAMDOCK_N1 = 0x1011
+    USB_PID_STREAMDOCK_N4PRO = 0x1008
+    USB_PID_STREAMDOCK_N4PROE = 0x1021
+
 
 from .Devices.StreamDock293 import StreamDock293
 from .Devices.StreamDock293s import StreamDock293s
@@ -39,13 +46,27 @@ from .Devices.StreamDock293V3 import StreamDock293V3
 from .Devices.StreamDockN3 import StreamDockN3
 from .Devices.StreamDockN4 import StreamDockN4
 from .Devices.StreamDockN1 import StreamDockN1
+from .Devices.StreamDockN4Pro import StreamDockN4Pro
+
 g_products = [
     # 293 serial
     (USBVendorIDs.USB_VID_293, USBProductIDs.USB_PID_STREAMDOCK_293, StreamDock293),
     (USBVendorIDs.USB_VID_293s, USBProductIDs.USB_PID_STREAMDOCK_293s, StreamDock293s),
-    (USBVendorIDs.USB_PID_293V3, USBProductIDs.USB_PID_STREAMDOCK_293V3, StreamDock293V3),
-    (USBVendorIDs.USB_PID_293V3, USBProductIDs.USB_PID_STREAMDOCK_293V3EN, StreamDock293V3),
-    (USBVendorIDs.USB_PID_293V3, USBProductIDs.USB_PID_STREAMDOCK_293V25, StreamDock293V3),
+    (
+        USBVendorIDs.USB_PID_293V3,
+        USBProductIDs.USB_PID_STREAMDOCK_293V3,
+        StreamDock293V3,
+    ),
+    (
+        USBVendorIDs.USB_PID_293V3,
+        USBProductIDs.USB_PID_STREAMDOCK_293V3EN,
+        StreamDock293V3,
+    ),
+    (
+        USBVendorIDs.USB_PID_293V3,
+        USBProductIDs.USB_PID_STREAMDOCK_293V25,
+        StreamDock293V3,
+    ),
     # N3
     (USBVendorIDs.USB_VIDN3, USBProductIDs.USB_PID_STREAMDOCK_N3, StreamDockN3),
     (USBVendorIDs.USB_VIDN3, USBProductIDs.USB_PID_STREAMDOCK_N3EN, StreamDockN3),
@@ -56,8 +77,18 @@ g_products = [
     # N4
     (USBVendorIDs.USB_VIDN4, USBProductIDs.USB_PID_STREAMDOCK_N4, StreamDockN4),
     (USBVendorIDs.USB_VIDN4EN, USBProductIDs.USB_PID_STREAMDOCK_N4EN, StreamDockN4),
-
-    #N1
+    # N1
     (USBVendorIDs.USB_VIDN1, USBProductIDs.USB_PID_STREAMDOCK_N1, StreamDockN1),
     (USBVendorIDs.USB_VIDN1EN, USBProductIDs.USB_PID_STREAMDOCK_N1EN, StreamDockN1),
+    # N4 Pro
+    (
+        USBVendorIDs.USB_VIDN4PRO,
+        USBProductIDs.USB_PID_STREAMDOCK_N4PRO,
+        StreamDockN4Pro,
+    ),
+    (
+        USBVendorIDs.USB_VIDN4PROE,
+        USBProductIDs.USB_PID_STREAMDOCK_N4PROE,
+        StreamDockN4Pro,
+    ),
 ]
